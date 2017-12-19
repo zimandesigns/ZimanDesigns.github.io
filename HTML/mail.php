f22557ed7dededc4c658fb69a88e2ec3fa6406bd
Ziman Designs <zimandesigns@gmail.com>
2017-12-18 19:57:27 -0500

    first commit

---
+++ HTML/mail.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="Contact form\n\nFrom: $name \nMessage: $message";
+$recipient = "zimandesigns@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You for your interest in Ziman Designs! Your e-mail was
+successfully sent and we will get back to you as soon as possible!";
+
+?>
